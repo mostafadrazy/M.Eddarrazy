2b7fa2b09ec000adb75defca2f4ce6c93780b520
Mustapha Eddarrazy <120674172+mostafadrazy@users.noreply.github.com>
2026-09-10 23:32:23 +0100

    Add GitHub URL to SOCIAL_LINKS

--- constants.ts
+++ constants.ts
@@ -5,6 +5,7 @@ export const SOCIAL_LINKS = {
   email: "Mostafadrazy@gmail.com",
   phone: "+212 657 067 384",
   linkedin: "https://linkedin.com/in/eddarrazy",
+  github: "https://github.com/mostafadrazy",
   website: "https://www.9adiya.site/",
   location: "Salé, Morocco"
 };
